ea98e99bfe19213b55fc3f1c3e40c1965ac5944a
Jasper Hugunin <jasper2011@hugunin.net>
2015-07-05 19:59:31 -0700

    Don't need heap in scheduler

--- schedule/jas1.py
+++ schedule/jas1.py
@@ -1,8 +1,5 @@
-from heap import Heap
-
 def Jas1Scheduler(object):
     def __init__(self, ratio=2, limit=4, baseInterval=10):
-        self.queue = Heap()
         self.lastTimes = {}
         self.ratio = ratio
         self.limit = limit
